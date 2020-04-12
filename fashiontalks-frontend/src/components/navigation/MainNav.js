81935c633eaf33f262bcf3877f5001171aa48703
jon <jonathan.wong110@gmail.com>
2020-04-12 14:48:59 -0400

    added a sign up link to the navbar, changed the background color, and rearranged the breaks in the post

--- fashiontalks-frontend/src/components/navigation/MainNav.js
+++ fashiontalks-frontend/src/components/navigation/MainNav.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react';
-import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
 class MainNav extends Component {
 
   render() {
-    
+
     return (
       <>
         <Navbar collapseOnSelect bg="dark" variant="dark" sticky="top" >
-            <Nav className="mr-auto">
-              <Link to="/" style={{ color: 'white', textDecoration: 'none' }} > FashionTalks </Link>
-            </Nav>
-            <Nav className="mr-auto">
-              <Link to="/clothing" style={{ color: 'white', textDecoration: 'none' }} > Clothing </Link>
-            </Nav>
-            <Nav className="mr-auto">
-              <Link to="/shoes" style={{ color: 'white', textDecoration: 'none' }} > Shoes </Link>
-            </Nav>
-            <Nav className="mr-auto">
-              <Link to="/entertainment" style={{ color: 'white', textDecoration: 'none' }} > Entertainment </Link>
-            </Nav>
-            <Nav className="mr-auto">
-              <Link to="/tech" style={{ color: 'white', textDecoration: 'none' }} > Tech </Link>
-            </Nav>
-            <Form inline className="mr-auto">
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            </Form>
-            <Nav id="login">
-              <Link to="signup" style={{ color: 'white', textDecoration: 'none' }} > Sign In </Link>
-            </Nav>
+          <Nav className="mr-auto">
+            <Link to="/" style={{ color: 'white', textDecoration: 'none' }} > FashionTalks </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/clothing" style={{ color: 'white', textDecoration: 'none' }} > Clothing </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/shoes" style={{ color: 'white', textDecoration: 'none' }} > Shoes </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/entertainment" style={{ color: 'white', textDecoration: 'none' }} > Entertainment </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/tech" style={{ color: 'white', textDecoration: 'none' }} > Tech </Link>
+          </Nav>
+          <Form inline className="mr-auto">
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Form>
+          <Nav id="login">
+            <Link to="/login" style={{ color: 'white', textDecoration: 'none' }} > Sign In </Link>
+          </Nav>
+          <Nav id="signup">
+            <Link to="/signup" style={{ color: 'white', textDecoration: 'none' }} > Sign Up </Link>
+          </Nav>
         </Navbar>
-        <br></br>
-        <br></br>
       </>
     )
   }
--- fashiontalks-frontend/src/components/posts/Post.js
+++ fashiontalks-frontend/src/components/posts/Post.js
@@ -2,19 +2,21 @@ import React from 'react'
 import { Card } from 'react-bootstrap';
 
 export default function Post(props) {
-    
+
     let { post } = props
 
     return (
-        <Card style={{ height: '200px', margin: '20px' }}>
-            <Card.Body>
-                <Card.Title>{post.title}</Card.Title>
-                <Card.Text>
-                    {post.body}
-                </Card.Text>
-            </Card.Body>
+        <>
             <br></br>
-        </Card>
+            <Card style={{ height: '200px', margin: '20px' }}>
+                <Card.Body>
+                    <Card.Title>{post.title}</Card.Title>
+                    <Card.Text>
+                        {post.body}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
     )
 
 }
