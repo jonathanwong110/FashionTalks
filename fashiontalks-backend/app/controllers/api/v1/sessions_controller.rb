f48132d9bde5a75e16dbb98d98327a586b09df92
jon <jonathan.wong110@gmail.com>
2020-04-19 14:33:04 -0400

    fixed indentation in the sessions controller, inserted how many comments are shown in a post with custom css

--- fashiontalks-backend/app/controllers/api/v1/sessions_controller.rb
+++ fashiontalks-backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
 
-    def create
-        user = User.find_by(username: params[:session][:username])
-        if user && user.authenticate(params[:session][:password])
-            session[:user_id] = user.id
-            redirect_to users_path
-        else
-            flash[:message] = "Invalid Username and/or Password. Please try again."
-            render :new
-        end
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to users_path
+    else
+      flash[:message] = "Invalid Username and/or Password. Please try again."
+      render :new
     end
-    
-    def destroy
-      if session[:user_id] == current_user.id
-        session.clear
-        redirect_to root_url
-      end
+  end
+  
+  def destroy
+    if session[:user_id] == current_user.id
+      session.clear
+      redirect_to root_url
     end
+  end
 end
