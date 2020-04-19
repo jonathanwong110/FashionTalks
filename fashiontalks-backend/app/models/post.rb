b95420fb11ab991309bcbc9002427234e505d859
jon <jonathan.wong110@gmail.com>
2020-04-18 23:00:03 -0400

    added validations to the user, post, and comment model

--- fashiontalks-backend/app/models/post.rb
+++ fashiontalks-backend/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
     belongs_to :section
     has_many :comments, through: :user
+
+    validates_presence_of :title, :body
+
 end
--- fashiontalks-backend/app/models/user.rb
+++ fashiontalks-backend/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :posts
     has_many :comments
+
+    validates_presence_of :username, :email, :password
+
+    validates :username, uniqueness: { case_sensitive: false }
+
 end
