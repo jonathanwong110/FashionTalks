6071c54ad6402396e8c1a71583815b6778560f1e
jon <jonathan.wong110@gmail.com>
2020-05-03 00:59:45 -0400

    created a current user action file

---
+++ fashiontalks-frontend/src/actions/currentUserAction.js
@@ -0,0 +1,12 @@
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
